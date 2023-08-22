69ccfa575379114eb1a9f5dbb5197274aa79477f
admin <501846822@qq.com>
2023-08-22 22:32:18 +0800

    大唐：添加漏采数据正则规则 华电：获取列表页数据

--- sites/PhaseFour/华电采购/chdtp_com_jzxtpbg.py
+++ sites/PhaseFour/华电采购/chdtp_com_jzxtpbg.py
@@ -46,28 +46,45 @@ class BidZGDZ(TaskBase):
         self.collection_name = 'chdtp_com_jzxtpbg'
         self.key_field = 'article_url'
 
-    def get_cookies(self, content, url):
-        self.log.error("error response, {}, url:{}".format(content[0], url))
+    def get_cookies_and_content(self, url):
         with sync_playwright() as playwright:
             browser = playwright.chromium.launch(headless=False)
             context = browser.new_context()
             page = context.new_page()
+            js = """
+                Object.defineProperties(navigator, {webdriver:{get:()=>undefined}});
+            """
+            page.add_init_script(js)  # 执行规避webdriver检测
             page.goto(url)
             page.wait_for_load_state("networkidle")
+            try:
+                # 检测滑块，滑动滑块
+                slider = page.locator('xpath=//span[@id="nc_1_n1z"]').bounding_box()
+                page.mouse.move(x=slider['x'], y=slider['y'] + slider['height'] / 2)
+                page.mouse.down()
+                time.sleep(random.randint(3, 5))
+                page.mouse.move(x=slider['x'] + random.randint(380, 420), y=slider['y'] + slider['height'] / 2)
+                page.mouse.up()
+                # page.reload()
+                page.wait_for_load_state("networkidle")
+                time.sleep(random.randint(3, 5))
+            except Exception as e:
+                pass
             storage_state = context.storage_state()
             cookie = ''
             for cookie_info in storage_state['cookies']:
                 cookie_text = cookie_info['name'] + '=' + cookie_info['value']
                 cookie += cookie_text + ';'
             self.headers['Cookie'] = cookie.rstrip(';')
+            # Get_the_data(page.content())
+            content = page.content()
             context.close()
             browser.close()
-
-        # return content, url
+            return content
 
     def run(self):
         TIMEOUT = 60
-        url = "https://www.chdtp.com/pages/wzglS/cgxx/caigou.jsp?cgtype=2"
+        url = "https://www.chdtp.com/webs/displayNewsCgxxAction.action?cgggtype=0"
         self.headers = {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'Accept-Encoding': 'gzip, deflate, br',
@@ -75,7 +92,7 @@ class BidZGDZ(TaskBase):
             'Cache-Control': 'max-age=0',
             'Connection': 'keep-alive',
             'Host': 'www.chdtp.com',
-            'Referer': 'https://www.chdtp.com/webs/displayNewsCgxxAction.action?cgggtype=0',
+            'Referer': 'https://www.chdtp.com/pages/wzglS/homepage/index.jsp',
             'Sec-Ch-Ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
             'Sec-Ch-Ua-Mobile': '?0',
             'Sec-Ch-Ua-Platform': "Windows",
@@ -86,135 +103,172 @@ class BidZGDZ(TaskBase):
             'Upgrade-Insecure-Requests': '1',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.3'
         }
+        # with sync_playwright() as playwright:
+        #     browser = playwright.chromium.launch(headless=False)
+        #     context = browser.new_context()
+        #     page = context.new_page()
+        #     js = """
+        #         Object.defineProperties(navigator, {webdriver:{get:()=>undefined}});
+        #     """
+        #     page.add_init_script(js)  # 执行规避webdriver检测
+        #     for i in range(2):
+        #         page.goto(url)
+        #         page.wait_for_load_state("networkidle")
+        #         page.reload()
+        #         time.sleep(3)
+        #     storage_state = context.storage_state()
+        #     cookie = ''
+        #     for cookie_info in storage_state['cookies']:
+        #         cookie_text = cookie_info['name'] + '=' + cookie_info['value']
+        #         cookie += cookie_text + ';'
+        #     self.headers['Cookie'] = cookie.rstrip(';')
+        #     context.close()
+        #     browser.close()
+        #
+        # url_base = 'https://www.chdtp.com/webs/displayNewsCgxxAction.action?cgggtype=0'
+        #
+        # url = url_base.format(1)
+        # self.log.info("开始采集第1页：{}".format(url))
+        # # content = self.req(url=url, req_type='post', headers=self.headers, data=form_data, timeout=TIMEOUT,
+        # #                    verify=False)
+        # self.max_retries = 1
+        # content = self.req(url=url, headers=self.headers, timeout=TIMEOUT)
+        # # data = {
+        # #     'cgggtype': "0",
+        # #     'jump': "2",
+        # #     'page.pageSize': "20",
+        # #     'page.currentpage': "1",
+        # #     'page.totalCount': "37787"
+        # # }
+        # # url = 'https://www.chdtp.com/webs/displayNewsCgxxAction.action'
+        # # data = json.dumps(data)
+        # # content = self.req(url=url, req_type='post', headers=self.headers, data=data, timeout=TIMEOUT, verify=False)
+        # while True:
+        #     if isinstance(content, tuple):
+        #         if content[0] == 412 or content[0] == 400:
+        #             self.get_cookies_and_content(url)
+        #             content = self.req(url=url, headers=self.headers, timeout=TIMEOUT)
+        #         else:
+        #             self.log.info("未找到：" + url)
+        #             return
+        #     else:
+        #         break
+        # if not content:
+        #     return
+        # html = etree.HTML(content)
+        # all_re_page = html.xpath('string(//span[@class="page"])')
+        # total = html.xpath('string(//span[@class="page"])')
+        # all_re_page = util.re_find_one('(?<=第).*?(?=页，)', all_re_page)
+        # all_re_page = util.re_find_one('[^/]+(?!.*/)', all_re_page)
+        # total = util.re_find_one('(?<=共).*?(?=条记录)', total)
+        # total = int(total)
+        # if all_re_page:
+        #     all_re_page = int(all_re_page)
+        # else:
+        #     all_re_page = 1
+        # pages = all_re_page
+        # self.log.info("总页数：{},开始采集第1页：{}".format(all_re_page, url))
+        # self.list_parse(content, url)
+        # for num in range(2, pages + 1):
+        #     data = {
+        #         'cgggtype': "0",
+        #         'jump': str(num-1) if num-1 > 0 else "1",
+        #         'page.pageSize': "20",
+        #         'page.currentpage': str(num),
+        #         'page.totalCount': str(total)
+        #     }
+        #     url = 'https://www.chdtp.com/webs/displayNewsCgxxAction.action'
+        #     data = json.dumps(data)
+        #     self.log.info("总页数：{},开始采集第{}页：{}".format(all_re_page, num, url))
+        #     content = self.req(url=url, req_type='post', headers=self.headers, data=data, timeout=TIMEOUT, verify=False)
+        #     if isinstance(content, tuple):
+        #         if content[0] == 412:
+        #             self.get_cookies_and_content(url)
+        #             content = self.req(url=url, req_type='post', headers=self.headers, data=data, timeout=TIMEOUT,
+        #                                verify=False)
+        #         else:
+        #             self.log.info("未找到：" + url)
+        #             continue
+        #     self.list_parse(content, url)
+        # self.log.info("{} 数据采集完毕！".format(self.file_name))
+
         with sync_playwright() as playwright:
-            browser = playwright.chromium.launch(headless=False)
+            browser = playwright.firefox.launch(headless=False)
             context = browser.new_context()
             page = context.new_page()
             js = """
                 Object.defineProperties(navigator, {webdriver:{get:()=>undefined}});
             """
             page.add_init_script(js)  # 执行规避webdriver检测
-            for i in range(3):
-                page.goto(url)
-                page.wait_for_load_state("networkidle")
-                page.reload()
-                time.sleep(3)
+            page.goto(url)
+            page.wait_for_load_state("networkidle")
             storage_state = context.storage_state()
             cookie = ''
             for cookie_info in storage_state['cookies']:
                 cookie_text = cookie_info['name'] + '=' + cookie_info['value']
                 cookie += cookie_text + ';'
             self.headers['Cookie'] = cookie.rstrip(';')
+            # Get_the_data(page.content())
+            content = page.content()
+
+            while True:
+                next_page_flag = self.list_parse(content, url)
+                time.sleep(random.randint(60, 120))
+                if next_page_flag:
+                    page.locator('xpath=//span[@class="page"]/input[3]').click()
+                    page.wait_for_load_state("networkidle")
+                else:
+                    break
+
             context.close()
             browser.close()
-
-        url_base = 'https://www.chdtp.com/webs/displayNewsCgxxAction.action?cgggtype=0'
-
-        url = url_base.format(1)
-        self.log.info("开始采集第1页：{}".format(url))
-        # content = self.req(url=url, req_type='post', headers=self.headers, data=form_data, timeout=TIMEOUT,
-        #                    verify=False)
-        content = self.req(url=url, headers=self.headers, timeout=TIMEOUT)
-        # data = {
-        #     'cgggtype': "0",
-        #     'jump': "2",
-        #     'page.pageSize': "20",
-        #     'page.currentpage': "1",
-        #     'page.totalCount': "37787"
-        # }
-        # url = 'https://www.chdtp.com/webs/displayNewsCgxxAction.action'
-        # data = json.dumps(data)
-        # content = self.req(url=url, req_type='post', headers=self.headers, data=data, timeout=TIMEOUT, verify=False)
-        while True:
-            if isinstance(content, tuple):
-                if content[0] == 412 or content[0] == 400:
-                    self.get_cookies(content, url)
-                    content = self.req(url=url, headers=self.headers, timeout=TIMEOUT)
-                else:
-                    self.log.info("未找到：" + url)
-                    return
-            else:
-                break
-        if not content:
-            return
-        html = etree.HTML(content)
-        all_re_page = html.xpath('string(//span[@class="page"])')
-        total = html.xpath('string(//span[@class="page"])')
-        all_re_page = util.re_find_one('(?<=第).*?(?=页，)', all_re_page)
-        all_re_page = util.re_find_one('[^/]+(?!.*/)', all_re_page)
-        total = util.re_find_one('(?<=共).*?(?=条记录)', total)
-        total = int(total)
-        if all_re_page:
-            all_re_page = int(all_re_page)
-        else:
-            all_re_page = 1
-        pages = all_re_page
-        self.log.info("总页数：{},开始采集第1页：{}".format(all_re_page, url))
-        self.list_parse(content, url)
-        for num in range(2, pages + 1):
-            data = {
-                'cgggtype': "0",
-                'jump': str(num-1) if num-1 > 0 else "1",
-                'page.pageSize': "20",
-                'page.currentpage': str(num),
-                'page.totalCount': str(total)
-            }
-            url = 'https://www.chdtp.com/webs/displayNewsCgxxAction.action'
-            data = json.dumps(data)
-            self.log.info("总页数：{},开始采集第{}页：{}".format(all_re_page, num, url))
-            content = self.req(url=url, req_type='post', headers=self.headers, data=data, timeout=TIMEOUT, verify=False)
-            if isinstance(content, tuple):
-                if content[0] == 412:
-                    self.get_cookies(content, url)
-                    content = self.req(url=url, req_type='post', headers=self.headers, data=data, timeout=TIMEOUT,
-                                       verify=False)
-                else:
-                    self.log.info("未找到：" + url)
-                    continue
-            self.list_parse(content, url)
-        self.log.info("{} 数据采集完毕！".format(self.file_name))
 
     def list_parse(self, maincontent, url):
         urlbase = 'https://www.chdtp.com/staticPage/'
         list_html = etree.HTML(maincontent)
         items = list_html.xpath('//form[@name="resultForm"]//table//tr')
         for item in items:
-            time.sleep(random.randint(10, 15))
             if self.exit_flag:
                 return
             title = item.xpath("string(./td[2]/a[1]/@title)")
             href_text = item.xpath("string(./td[2]/a[1]/@href)")
             href = util.re_find_one("'(.*?)'", href_text)
             detail_url = urljoin(urlbase, href)
-            detail_content = self.req(url=detail_url, headers=self.headers)
-            if isinstance(detail_content, tuple):
-                if detail_content[0] == 412:
-                    self.get_cookies(detail_content, url)
-                    detail_content = self.req(url=detail_url, headers=self.headers)
-                else:
-                    self.log.info("未找到：" + url)
-                    continue
-            if not detail_content or isinstance(detail_content, tuple):
-                self.log.error("{} no detail_content".format(detail_url))
-                continue
-            source_code = detail_content
-            detail_content = re.sub("<script[\s\S]*?/script>", "", detail_content, flags=re.I)
-            detail_content = re.sub("<style[\s\S]*?/style>", "", detail_content, flags=re.I)
-            html = etree.HTML(detail_content)
-            html1 = etree.HTML(re.sub('</p>', '</p>\n', detail_content))
+            # detail_content = self.req(url=detail_url, headers=self.headers)
+            # if isinstance(detail_content, tuple):
+            #     if detail_content[0] == 412:
+            #         self.get_cookies_and_content(url)
+            #         detail_content = self.req(url=detail_url, headers=self.headers)
+            #     else:
+            #         self.log.info("未找到：" + url)
+            #         continue
+            # if not detail_content or isinstance(detail_content, tuple):
+            #     self.log.error("{} no detail_content".format(detail_url))
+            #     continue
+            # source_code = detail_content
+            # detail_content = re.sub("<script[\s\S]*?/script>", "", detail_content, flags=re.I)
+            # detail_content = re.sub("<style[\s\S]*?/style>", "", detail_content, flags=re.I)
+            # html = etree.HTML(detail_content)
+            # html1 = etree.HTML(re.sub('</p>', '</p>\n', detail_content))
             # content_text = html.xpath('string(//div[@class="detail_box qst_box"])')
-            content_text1 = html1.xpath('string(//div[@class="box"]/div/table)')
+            # content_text1 = html1.xpath('string(//div[@class="box"]/div/table)')
             data = {}
             publish_time = item.xpath('string(./td[4])').replace('[', '').replace(']', '')
             data['article_url'] = detail_url
             data['标题'] = title
             data['时间'] = publish_time
-            data['正文'] = content_text1.strip()
-            data['源码'] = str(source_code).strip()
+            # data['正文'] = content_text1.strip()
+            # data['源码'] = str(source_code).strip()
             data['所属频道'] = self.file_name
             # data[TABField.announcement] = item.xpath("string(./@title)")
-            done_fields = []
-            self.detail_parse(detail_content, detail_url, data, done_fields=done_fields)
+            # done_fields = []
+            # self.detail_parse(detail_content, detail_url, data, done_fields=done_fields)
+            self.upload(data)
+
+        if not list_html.xpath('//span[@class="page"]/input[@src="images/page/page-next.jpg" and @disabled]'):
+            return True
+        else:
+            return False
 
     def detail_parse(self, detail_content, detail_url, data=None, done_fields=None):
         # print(data)
@@ -273,7 +327,7 @@ class BidZGDZ(TaskBase):
 
 if __name__ == '__main__':
     params = {
-        "proxy_flag": True,
+        "proxy_flag": False,
         "query_time": "",
         "MainKeys": [
             ""
